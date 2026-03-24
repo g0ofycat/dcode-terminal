9e3ee0ae69db756c6f0ce9d148a572913a95aa2d
g0ofycat <g0ofycatbusiness@gmail.com>
2026-03-24 16:23:18 -0400

    better notes

--- usage/main.cpp
+++ usage/main.cpp
@@ -8,17 +8,23 @@
 static void print_usage()
 {
     std::cout <<
-        "usage: dcode -i <input> <ops...> [--e|--d]\n"
+        "dcode - data encoder/decoder\n"
         "\n"
-        "notes: last flags for encoding and decoding override normal ops (default is encoding). you can specify if you want encoded or decoded ops manually by doing something like -b64=e OR -b64=d and etc. input and ops position are interchangeable (they can be anywhere). manual coding ops for specific flags override the last flag [--e|--d]"
+        "Usage:\n"
+        "  dcode -i <input> [ops...] [--e|--d]\n"
         "\n"
-        "mode flags:\n"
-        "  --e          encode\n"
-        "  --d          decode\n"
+        "Ops:\n"
+        "  -b64        base64\n"
+        "  -r          reverse\n"
         "\n"
-        "pipeline ops:\n"
-        "  -b64         base64\n"
-        "  -r           reverse\n"
+        "Modes:\n"
+        "  --e         encode (default)\n"
+        "  --d         decode\n"
+        "\n"
+        "Rules:\n"
+        "  last --e/--d wins\n"
+        "  op=e|d overrides mode\n"
+        "  order of input/ops doesn't matter\n"
         "\n";
 }
 
